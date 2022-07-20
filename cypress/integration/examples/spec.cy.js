901d32e625914ff80d51c268de7cb854fc66343d
Irmak Erdem <irmakerdem9@gmail.com>
2022-07-20 11:13:00 -0600

    Set up test 3 blocks

---
+++ cypress/integration/examples/spec.cy.js
@@ -0,0 +1,21 @@
+describe('Turing Cafe Reservations', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+    .contains('Turing Cafe Reservations')
+  });
+
+  it('Should display __________ on page load ', () => {
+    
+  });
+
+  it('Should be able to update form based on user input ', () => {
+    // Write a test that checks that when data is put into the form, the value is reflected in that form input.
+    
+  });
+
+  it('Should be able to display new reservation on page ', () => {
+    // Write a test to check the user flow of adding a new reservation to the page.
+    
+  });
+
+
