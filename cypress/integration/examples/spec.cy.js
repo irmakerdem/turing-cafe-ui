bb5aa762d5de7e09ad1dbdf6c3e29aa33b3a00e9
Irmak Erdem <irmakerdem9@gmail.com>
2022-07-20 11:27:27 -0600

    Add 3rd test

--- cypress/integration/examples/spec.cy.js
+++ cypress/integration/examples/spec.cy.js
@@ -9,6 +9,7 @@ describe('Turing Cafe Reservations', () => {
     cy.get('button').should('be.visible')
     cy.get('.resy-container > :nth-child(1) > :nth-child(1)').contains('Christie')
     cy.get(':nth-child(1) > :nth-child(1) > button').should('be.visible')
+    cy.get('.resy-container').find('.card').should('have.length', '9')
   });
 
   it('Should be able to update form based on user input', () => {
@@ -19,9 +20,15 @@ describe('Turing Cafe Reservations', () => {
     cy.get('[type="number"]').type(2)
   });
 
-  it.only('Should be able to display new reservation on page', () => {
+  it('Should be able to display new reservation on page', () => {
     // Write a test to check the user flow of adding a new reservation to the page.
-
+    cy.get('[placeholder="Name"]').type('Irmak')
+    cy.get('[placeholder="Date (mm/dd)"]').type('07/20')
+    cy.get('[placeholder="Number of guests"]').type('5')
+    cy.get('[type="number"]').type(2)
+    cy.get('.submit').click()
+    cy.get('.resy-container > :nth-child(1) > :nth-child(10)').should('be.visible')
+    cy.get('.resy-container').find('.card').should('have.length', '10')
   });
 
 })
