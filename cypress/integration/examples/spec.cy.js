af7a4b1d8d73bcd089d6588f24388ae5ce231dfe
Irmak Erdem <irmakerdem9@gmail.com>
2022-07-20 11:18:30 -0600

    Add test for displaying page load

--- cypress/integration/examples/spec.cy.js
+++ cypress/integration/examples/spec.cy.js
@@ -4,18 +4,21 @@ describe('Turing Cafe Reservations', () => {
     .contains('Turing Cafe Reservations')
   });
 
-  it('Should display __________ on page load ', () => {
-    
+  it('Should display a log of reservations on page load', () => {
+    cy.get('form').should('be.visible')
+    cy.get('button').should('be.visible')
+    cy.get('.resy-container > :nth-child(1) > :nth-child(1)').contains('Christie')
+    cy.get(':nth-child(1) > :nth-child(1) > button').should('be.visible')
   });
 
-  it('Should be able to update form based on user input ', () => {
+  it('Should be able to update form based on user input', () => {
     // Write a test that checks that when data is put into the form, the value is reflected in that form input.
     
   });
 
-  it('Should be able to display new reservation on page ', () => {
+  it('Should be able to display new reservation on page', () => {
     // Write a test to check the user flow of adding a new reservation to the page.
     
   });
 
-
+})
