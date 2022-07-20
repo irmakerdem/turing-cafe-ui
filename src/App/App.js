2699fa30acc3b187a0b7ad39818eac05877b2efb
Irmak Erdem <irmakerdem9@gmail.com>
2022-07-20 10:54:04 -0600

    Add form to DOM

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Reservations from '../Reservations';
+import Form from '../Form';
 import './App.css';
 
 class App extends Component {
@@ -27,7 +28,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+        <Form />
         </div>
         <div className='resy-container'>
           <Reservations reservations={this.state.reservations}/>
---
+++ src/Form.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name:'',
+      date: '',
+      time: '',
+      number:''
+    }
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type='text'
+          placeholder='Name'
+          name='name'
+          value={this.state.name}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type='text'
+          placeholder='Date (mm/dd)'
+          name='date'
+          value={this.state.date}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type='text'
+          placeholder='Time'
+          name='time'
+          value={this.state.time}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type='text'
+          placeholder='Number of guests'
+          name='number'
+          value={this.state.number}
+          onChange={event => this.handleChange(event)}
+        />
+        <button className='submit' type='submit' onClick={event => this.submitReservation(event)}>Make Reservation</button>
+      </form>
+    )
+  }
+}
+
+export default Form;
